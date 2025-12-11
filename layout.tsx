e43ae14f9a9fa0ab9a58817b8165e568fb1ea35b
MitinWarface <mitinwarface@gmail.com>
2025-12-11 23:06:30 +0300

    main

--- layout.tsx
+++ layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import ThemeToggle from '@/components/ThemeToggle';
+import ThemeToggle from '~/components/ThemeToggle';
 
 const inter = Inter({ subsets: ['latin'] });
 
