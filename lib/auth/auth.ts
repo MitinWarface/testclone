6d28fdf3a4fdf0931e8f0d5db5bcdc0201cc20a7
MitinWarface <mitinwarface@gmail.com>
2025-12-12 00:03:25 +0300

    main

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,18 +1,10 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '../../src/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+// Удаляем объявление глобальной переменной, чтобы избежать конфликта типов
+const prisma = new PrismaClient();
 import bcrypt from 'bcrypt';
 
 
